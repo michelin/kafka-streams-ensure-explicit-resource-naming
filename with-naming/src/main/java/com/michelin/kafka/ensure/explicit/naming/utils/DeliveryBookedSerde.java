ee2b6c2a23c20640e5f44686750c99692b46c9cc
Sebastien Viale <sebastien.viale@michelin.com>
2025-09-08 11:38:59 +0200

    rework modules (#14)
    * rework modules
    
    * fix module dependency

--- with-naming/src/main/java/com/michelin/kafka/ensure/explicit/naming/utils/DeliveryBookedSerde.java
+++ with-naming/src/main/java/com/michelin/kafka/ensure/explicit/naming/utils/DeliveryBookedSerde.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.michelin.kafka.commons;
+package com.michelin.kafka.ensure.explicit.naming.utils;
 
 import com.google.gson.Gson;
 import java.nio.charset.StandardCharsets;
